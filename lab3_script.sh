cf97858a6d61781ac0be2d41147b9804f7cc9f50
bryceskdev <bryceskdev@gmail.com>
2020-02-12 10:39:04 -0700

    Script File and Resaults

--- lab3_script.sh
+++ lab3_script.sh
@@ -18,3 +18,10 @@ r2=@
 echo "Number of Emails"
 egrep -c $r2 $file
 
+r3=^303-[0-9]{3}-[0-9]{4}
+echo "Numbers with 303"
+egrep -0 $r3 $file
+
+r4=$"@geocities.com"
+echo "Emails with geocities"
+grep $r4 $file >> email_results.txt
